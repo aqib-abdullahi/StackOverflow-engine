f451afdc3ba740e41363261b0e0ebf344b62680c
aqib-abdullahi <abdullahiaqib@gmail.com>
2024-11-09 00:29:04 +0100

    fixed module path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('stackoverflow_engine.urls'))
+    path('', include('backend.stackoverflow_engine.urls'))
 ]
