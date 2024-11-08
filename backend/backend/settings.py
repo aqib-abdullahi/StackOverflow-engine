a9a91d3dd07a56690d59691a3194660129e42e25
aqib-abdullahi <abdullahiaqib@gmail.com>
2024-11-09 00:18:06 +0100

    path ccorrected

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stackoverflow_engine',
+    'backend.stackoverflow_engine',
     'corsheaders',
     'rest_framework',
 ]
--- backend/stackoverflow_engine/apps.py
+++ backend/stackoverflow_engine/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class StackoverflowEngineConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stackoverflow_engine'
+    name = 'backend.stackoverflow_engine'
