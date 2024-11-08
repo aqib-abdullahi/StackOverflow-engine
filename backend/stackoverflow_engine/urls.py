d6f185c020110ceb743ca025bc9d6a5b2503c2b2
aqib-abdullahi <abdullahiaqib@gmail.com>
2024-11-09 00:33:10 +0100

    fixed module name

--- backend/stackoverflow_engine/urls.py
+++ backend/stackoverflow_engine/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from stackoverflow_engine.views import StackOverflowSearch
+from backend.stackoverflow_engine.views import StackOverflowSearch
 
 urlpatterns = [
     path('api/search/', StackOverflowSearch.as_view(), name='stackoverflow_search')    
